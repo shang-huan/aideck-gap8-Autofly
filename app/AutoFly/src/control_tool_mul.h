141242ce2f28fe6899159a89d2e4250968c2b2a9
shang-huan <2629045949@qq.com>
2023-10-07 16:19:54 +0800

    debug with server

--- app/AutoFly/src/control_tool_mul.h
+++ app/AutoFly/src/control_tool_mul.h
@@ -9,9 +9,8 @@ void UpdateMap(octoMap_t *octoMap, mapping_req_payload_t *mappingRequestPayload,
 void CalCandidates(coordinateF_t *candidates, example_measure_t *measurement, coordinateF_t *current_F);
 bool CalBestCandinates(octoMap_t *octoMap,uavControl_t* uavControl,uavControl_t** uavs);
 bool JumpLocalOp(uavControl_t *uavControl,uavControl_t ** uavs);
-bool UpdateLoops(uavControl_t* uavControl)
 bool CalNextPoint(uavControl_t* uavControl,uavControl_t** uavs,octoMap_t* octoMap);
 
-double CalMinDistance(uavControl_t* uavControl,uavControl_t** uavs, coordinateF_t* point)
+double CalMinDistance(uavControl_t* uavControl,uavControl_t** uavs, coordinateF_t* point);
 double CalAvoidWeight(float distance);
 #endif /* CONTROL_TOOL_MUL_H_ */
